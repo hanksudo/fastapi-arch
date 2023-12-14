ce2a5992cbef0f868e8c3a1f63c66db9a6d0bf65
Hank Wang <467745+hanksudo@users.noreply.github.com>
2023-12-14 18:00:09 +0900

    feat: add uvicorn

--- run.sh
+++ run.sh
@@ -2,4 +2,6 @@
 
 PROJECT_PATH=$(pwd)/app
 
-uvicorn main:app --port 3000 --reload --app-dir $PROJECT_PATH --reload-dir $PROJECT_PATH
+export PYTHONPATH=.
+
+poetry run uvicorn main:app --port 8000 --reload --app-dir $PROJECT_PATH --reload-dir $PROJECT_PATH
