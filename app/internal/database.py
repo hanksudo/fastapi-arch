649ed38def2b7c1935fd7342910b618e35d8a06c
Hank Wang <467745+hanksudo@users.noreply.github.com>
2024-01-09 16:05:44 +0900

    move database

--- app/internal/database.py
+++ app/internal/database.py
@@ -1,32 +1,32 @@
 from collections.abc import Generator
 from contextlib import contextmanager
-from typing import Any
+import logging
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session, scoped_session, sessionmaker
+from sqlalchemy.orm import DeclarativeBase, Session, scoped_session, sessionmaker
 
+logger = logging.getLogger(__name__)
+
+class Base(DeclarativeBase):
+    pass
 
 class DatabaseManager:
     def __init__(self, db_url: str) -> None:
-        self._engine = create_engine(db_url, echo=True)
+        self._engine = create_engine(db_url, echo=False)
         self._session_factory = scoped_session(
             sessionmaker(autocommit=False, autoflush=False, bind=self._engine)
         )
 
-    def init_db(self, Base: Any) -> None:
+    def init_db(self) -> None:
         Base.metadata.create_all(bind=self._engine)
 
     @contextmanager
-    def get_db(self) -> Generator[Session, None, None]:
-        db = self._session_factory()
+    def session(self) -> Generator[Session, None, None]:
+        session = self._session_factory()
         try:
-            yield db
+            yield session
         except Exception:
-            db.rollback()
+            session.rollback()
             raise
         finally:
-            db.close()
-
-    def get_db_deps(self) -> Generator[Session, None, None]:
-        with self.get_db() as db:
-            yield db
+            session.close()
