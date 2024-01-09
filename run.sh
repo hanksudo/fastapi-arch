dc4f09b39ac8c34cfb348aea9bfaea77d7ae5340
Hank Wang <467745+hanksudo@users.noreply.github.com>
2024-01-09 16:30:18 +0900

    format

--- run.sh
+++ run.sh
@@ -4,4 +4,8 @@ PROJECT_PATH=$(pwd)/app
 
 export PYTHONPATH=.
 
-poetry run uvicorn main:app --port 8000 --reload --app-dir $PROJECT_PATH --reload-dir $PROJECT_PATH
+poetry run uvicorn main:app \
+    --port 8000 \
+    --reload \
+    --app-dir $PROJECT_PATH \
+    --reload-dir $PROJECT_PATH
